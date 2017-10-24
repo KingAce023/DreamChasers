068ad7b7c4c45ff106cd6ccd2005e7a525f1573c
Sully Al Shemari <soltanalshemari@cmail.carleton.ca>
2017-10-24 18:44:49 -0400

    Add files via upload

---
+++ Navigate/settingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  settingsViewController.swift
+//  Navigate
+//
+//  Created by Sully Al Shemari on 2017-10-24.
+//  Copyright © 2017 Dream Chasers. All rights reserved.
+//
+
+import UIKit
+
+class settingsViewController: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.target = revealViewController()
+        menuBtn.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
