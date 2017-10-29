049be10e96be7eff323558c9755dc515770c5312
terryjiang99 <terry_jiang6@hotmail.com>
2017-10-28 20:08:57 -0400

    Add to Inventory Function

--- Navigate/DetailViewController.swift
+++ Navigate/DetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class DetailViewController: UIViewController {
 
@@ -18,14 +19,17 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imageNameLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var partAdded: UILabel!
     
-    
+    var dbref: DatabaseReference?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        dbref = Database.database().reference()
         imageNameLabel.text = dataModel.imageName
         yearLabel.text = dataModel.imageYear
         priceLabel.text = dataModel.imagePrice
+        self.partAdded.isHidden = true
+        partAdded.text = "Part Added!"
     
         imageView.image = UIImage(named: dataModel.imageName)
     }
@@ -35,6 +39,12 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func addToInventory(_ sender: Any) {
+        let user = Auth.auth().currentUser?.uid
+    self.dbref?.child("Users").child(user!).child("repairs").childByAutoId().setValue(imageNameLabel.text)
+        self.partAdded.isHidden = false
+        
+    }
     /*
     // MARK: - Navigation
 
